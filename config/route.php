13d17b101a7cb6d0cb433b0969d72c86abafadb6
alexlabuz <alex.labuz@live.fr>
2020-05-27 13:18:23 +0000

    Protection des pages d'administrations

--- config/route.php
+++ config/route.php
@@ -2,24 +2,25 @@
 
 function getPage($db){
 
-    $lesPages['accueil'] = "accueilControleur";
-    $lesPages['contact'] = "contactControleur";
-    $lesPages['connection'] = "connectionControleur";
-    $lesPages['inscrire'] = "inscrireControleur";
-    $lesPages['produit'] = "produitControleur";
-    $lesPages['liste'] = "listeControleur";
-    $lesPages['maintenance'] = "maintenanceControleur";
-    $lesPages['deconnexion'] = "deconnexionControleur";
-    $lesPages['utilisateur'] = "utilisateurControleur";
-    $lesPages['type'] = "typeControleur";
-    $lesPages['typeModif'] = "typeModifControleur";
-    $lesPages['produit-admin'] = "listeProduitControleur";
-    $lesPages['ajoutProduit'] = "ajoutProduit";
-    $lesPages['utilisateurModif'] = "utilisateurModifControleur";
-    $lesPages['produitModif'] = "produitModifControleur";
-    $lesPages['recherche'] = "rechercheControleur";
+    $lesPages['accueil'] = "accueilControleur;0";
+    $lesPages['contact'] = "contactControleur;0";
+    $lesPages['connection'] = "connectionControleur;0";
+    $lesPages['inscrire'] = "inscrireControleur;0";
+    $lesPages['maintenance'] = "maintenanceControleur;0";
+    $lesPages['produit'] = "produitControleur;0";
+    $lesPages['liste'] = "listeControleur;0";
+    $lesPages['deconnexion'] = "deconnexionControleur;0";
+    $lesPages['utilisateur'] = "utilisateurControleur;1";
+    $lesPages['type'] = "typeControleur;1";
+    $lesPages['typeModif'] = "typeModifControleur;1";
+    $lesPages['produit-admin'] = "listeProduitControleur;1";
+    $lesPages['ajoutProduit'] = "ajoutProduit;1";
+    $lesPages['utilisateurModif'] = "utilisateurModifControleur;1";
+    $lesPages['produitModif'] = "produitModifControleur;1";
+    $lesPages['recherche'] = "rechercheControleur;0";
 
     if($db != null){
+        // Vérifie si page existe dans GET
         if(isset($_GET["page"])){
             $page = $_GET['page'];
         }
@@ -27,12 +28,33 @@ function getPage($db){
             $page = "accueil";
         }
 
-        if(isset($lesPages[$page])){
-            $contenu = $lesPages[$page];
-        }
-        else{
+        // Teste si la page dans GET n'existe pas
+        if(!isset($lesPages[$page])){
             $contenu = $lesPages['accueil'];
         }
+
+        // Vérifie si l'utilisateur à le dro
+        $explode = explode(";", $lesPages[$page]);
+        $role = $explode[1];
+
+        if($role != 0){
+            if(isset($_SESSION["login"])){
+                if(isset($_SESSION["role"])){
+                    if($role != $_SESSION["role"]){
+                        $contenu = "accueilControleur";
+                    }else{
+                        $contenu = $explode[0];
+                    }
+                }else{
+                    $contenu = "accueilControleur";
+                }
+            }else{
+                $contenu = "accueilControleur";
+            }
+        }else{
+            $contenu = $explode[0];
+        }
+
     }else{
         $contenu = $lesPages["maintenance"];
     }
